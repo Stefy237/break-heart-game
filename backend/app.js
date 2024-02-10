4afdda614f14ac73137713d1bdc864f19da20430
Stefy237 <ngueganglaura@gmail.com>
2024-02-10 18:35:58 +0100

    test 6.3

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
-import {express} from 'express';
-import {bodyParser} from 'body-parser';
+/*const express = require("express");
+const bodyParser = require("body-parser");
 
-const login = require('./controllers/userControllers');
+const login = require("./controllers/userControllers");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/login', async(req, res)=>{
-    try {
-      await login(req, res);
-    } catch (error) {
-      console.error("Erreur lors de la connexion :", error);
-      res.status(500).json({ message: "Erreur lors de la connexion" });
-    }
-})
+app.post("/login", (req, res) => {
+  login(req, res).catch((error) => {
+    console.error("Erreur lors de la connexion :", error);
+    res.status(500).json({ message: "Erreur lors de la connexion" });
+  });
+});
+*/
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+
+const login = require("./controllers/userControllers");
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -10,7 +14,17 @@ app.get("/", (req, res) => {
   res.sendFile(filePath);
 });
 
-app.post("/process-form", (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/*app.post("/login", (req, res) => {
+  login(req, res).catch((error) => {
+    console.error("Erreur lors de la connexion :", error);
+    res.status(500).json({ message: "Erreur lors de la connexion" });
+  });
+});*/
+
+app.post("/login", (req, res) => {
   // Récupérer les données du formulaire
   const name = req.body.name;
   const pseudo = req.body.pseudo;
@@ -18,6 +32,10 @@ app.post("/process-form", (req, res) => {
   // Traitement des données du formulaire
   // ...
 
+  login(req, res).catch((error) => {
+    console.error("Erreur lors de la connexion :", error);
+    res.status(500).json({ message: "Erreur lors de la connexion" });
+  });
   // Rediriger l'utilisateur vers une autre page
   const rulesPath = path.join(__dirname, "../rules.html");
   res.redirect(rulesPath);
